1bcf8493519a38003cd39ba0808d37039a674183
ugwumadu116 <ugwumadu116@gmail.com>
2019-11-25 23:04:56 +0100

    testing auto deploy

--- stockruns/settings.py
+++ stockruns/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'lioear+t2m)w8tqh3!#=9keq+ldz^4dpe5h*a4k*5c=@%-y@nw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
